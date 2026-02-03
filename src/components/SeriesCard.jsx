ce94e5bca6f7cbc492bf3307403f326cbf7709c8
aisyahrr <aisyahr142@gmail.com>
2026-02-04 05:12:17 +0700

    seoul watch

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,12 +1,12 @@
 export default function SeriesCard({ item }) {
   return (
     <div className="flex flex-col gap-2 text-center">
-        <div className="relative w-full h-full rounded-xl overflow-hidden group cursor-pointer">
+        <div className="relative w-full h-[200px] sm:h-[210px] md:h-[250px] rounded-lg overflow-hidden group cursor-pointer">
         {/* Poster */}
         <img
             src={item.poster}
             alt={item.title}
-            className="w-full  object-cover 
+            className="w-full h-full object-cover 
                     transition-transform duration-300 
                     group-hover:scale-105"
         />
